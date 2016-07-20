c4cd51c3f64de8663a1efa626cba0883d287e6e1
Ryan Cosans <ryanmcosans@gmail.com>
2016-07-20 16:50:15 +0100

    added middleware to get the read the json file upon request

--- Startup.cs
+++ Startup.cs
@@ -10,30 +10,39 @@ namespace jsonmraz
 {
     public class Startup
     {
+
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             loggerFactory.AddConsole();
 
             if (env.IsDevelopment()) { app.UseDeveloperExceptionPage(); }
 
-            app.Map("/set", _app => {
+            string[] path = new string[] {};
+            string root = null;
+            dynamic json = null;
+
+            app.Use(async (context, next) => {
+                path = context.Request.Path.Value.Split('/');
+                root = path[1];
+
+                if (string.IsNullOrWhiteSpace(root)) return;
+                json = JsonConvert.DeserializeObject<dynamic>(File.ReadAllText($"json/{root}.json"));
+
+                await next();
+            });
+
+            app.MapWhen(context => context.Request.QueryString.Value.Contains("="), _app => {
                 _app.Run(async context => {
                     await context.Response.WriteAsync("set stuff");
                 });
             });
 
             app.Run(async context => {
-                var path = context.Request.Path.Value.Split('/');
-                var root = path[1];
 
                 if (string.IsNullOrWhiteSpace(root)) return;
 
-                var json = JsonConvert.DeserializeObject<dynamic>(File.ReadAllText($"json/{root}.json"));
-
                 foreach (var pathSection in path) {
-
                     if (string.IsNullOrWhiteSpace(pathSection) || pathSection == root) continue;
-
                     foreach (var jsonObj in json) {
                         json = jsonObj.Name == pathSection ? jsonObj.Value : json;
                     }
@@ -43,16 +52,11 @@ namespace jsonmraz
             });
         }
 
-        public static void Main(string[] args)
-        {
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
-                .UseStartup<Startup>()
-                .Build();
-
-            host.Run();
-        }
+        public static void Main(string[] args) => new WebHostBuilder()
+            .UseKestrel()
+            .UseContentRoot(Directory.GetCurrentDirectory())
+            .UseIISIntegration()
+            .UseStartup<Startup>()
+            .Build().Run();
     }
 }
