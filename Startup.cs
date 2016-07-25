d9bf62643bbfa6c6a96d948f34d2e76bd925e433
Ryan Cosans <ryanmcosans@gmail.com>
2016-07-25 16:25:00 +0100

    big ol' refactor and writing enabled

--- Startup.cs
+++ Startup.cs
@@ -1,10 +1,12 @@
+using System;
 using System.IO;
+using System.Linq;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
-// ReSharper disable All
+using Newtonsoft.Json.Linq;
 
 namespace jsonmraz
 {
@@ -16,38 +18,55 @@ namespace jsonmraz
 
             if (env.IsDevelopment()) { app.UseDeveloperExceptionPage(); }
 
-            string[] path = new string[] {};
+            string[] path = { };
             string root = null;
             dynamic json = null;
 
             app.Use(async (context, next) => {
                 path = context.Request.Path.Value.Split('/');
                 root = path[1];
-
                 if (string.IsNullOrWhiteSpace(root) || root.Contains("favicon.ico")) return;
-
-                json = JsonConvert.DeserializeObject<dynamic>(File.ReadAllText($"json/{root}.json"));
-
+                json = JObject.Parse(File.ReadAllText($"json/{root}.json"));
                 await next();
             });
 
-            app.MapWhen(context => context.Request.QueryString.Value.Contains("="), _app => {
+            app.MapWhen(context => context.Request.QueryString.Value.Contains("set"), _app => {
                 _app.Run(async context => {
-                    await context.Response.WriteAsync("set stuff");
+                    dynamic _json = findJsonObject(json, path, root);
+
+                    var propertyName = context.Request.Query.Single(x => x.Key == "set").Value[0];
+                    var propertyValue = context.Request.Query.Single(x => x.Key == "value").Value[0];
+
+                    var type = json.SelectToken($"{(_json as JObject)?.Path}")[propertyName].Value.GetType();
+
+                    json.SelectToken($"{(_json as JObject)?.Path}")[propertyName] = Convert.ChangeType(propertyValue, type);
+
+                    var jsonOutput = JsonConvert.SerializeObject(json);
+
+                    File.WriteAllText($"json/{root}.json", jsonOutput);
+
+                    await context.Response.WriteAsync($"{jsonOutput}");
                 });
             });
 
             app.Run(async context => {
-
-                foreach (var pathSection in path) {
-                    if (string.IsNullOrWhiteSpace(pathSection) || pathSection == root) continue;
-                    foreach (var jsonObj in json) {
-                        json = jsonObj.Name == pathSection ? jsonObj.Value : json;
-                    }
-                }
-
-                await context.Response.WriteAsync($"{JsonConvert.SerializeObject(json)}");
+                var jsonObj = findJsonObject(json, path, root);
+                await context.Response.WriteAsync($"{JsonConvert.SerializeObject(jsonObj)}");
             });
+        }
+
+        static dynamic findJsonObject(dynamic json, string[] path, string root)
+        {
+            var _json = json;
+            foreach (var pathSection in path) {
+
+                if (string.IsNullOrWhiteSpace(pathSection) || pathSection == root) continue;
+                foreach (var jsonObj in _json) {
+                    if (jsonObj.Name != pathSection) continue;
+                    _json = jsonObj.Value;
+                }
+            }
+            return _json;
         }
 
         public static void Main(string[] args) => new WebHostBuilder()
